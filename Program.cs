1c02ee61da38b7c60d52af4f7e01ab35ddda7811
hce <hce@VS2017>
2018-05-21 13:38:33 +0200

    initial version

--- Program.cs
+++ Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MultipIfElse
+namespace MultipleIfElse
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            const string criteria = "open";
+            switch (criteria)
+            {
+                case "open": Console.WriteLine("Open"); break;
+                case "close": Console.WriteLine("Close"); break;
+                case "print": Console.WriteLine("Print"); break;
+            }
+
+            IAction open = new Open();
+            open.DoWork();
+
+            Console.Read();
         }
     }
+
+    public interface IAction { void DoWork(); }
+    public class Open: IAction { public void DoWork() { Console.WriteLine("Open"); } }
+    public class Close: IAction { public void DoWork() { Console.WriteLine("Close"); } }
+    public class Print: IAction { public void DoWork() { Console.WriteLine("Print"); } }
 }
